6b2e2b88014d459e0df88042acf849c7b3b95be7
Juan Carlos de Jesus Gonzalez Lopez <97489315+juan12312121@users.noreply.github.com>
2025-02-20 17:49:57 -0700

    Resolve merge conflicts and add new files

--- src/app/compartidos/compartidos.component.ts
+++ src/app/compartidos/compartidos.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { PaginacionComponent } from '../paginacion/paginacion.component';
 import { SidebarComponent } from '../sidebar/sidebar.component';
+
 
 
 @Component({
   selector: 'app-compartidos',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent,PaginacionComponent],
   templateUrl: './compartidos.component.html',
   styleUrl: './compartidos.component.css'
 })
